7e38d02ea8de43138f29e96c185590dd37c927ef
Joker <569155228@qq.com>
2020-11-30 11:50:40 +0800

    <初步构建>

---
+++ Assets/Xlua/XluaFrame/XluaMgr.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XLua;
+public class XluaMgr : MonoBehaviour
+{
+    public LuaEnv luaEnv;
+    public void Awake()
+    {
+        
+    }
+
+    public void Start()
+    {
+        
+    }
+
+    public void Update()
+    {
+        
+    }
+}
