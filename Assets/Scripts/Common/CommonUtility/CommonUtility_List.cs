02f3a1dabda939fd3488bd527d634f543a60b0d7
Joker <569155228@qq.com>
2020-12-13 20:41:47 +0800

    <修改bug>

--- Assets/Scripts/Common/CommonUtility/CommonUtility_List.cs
+++ Assets/Scripts/Common/CommonUtility/CommonUtility_List.cs
@@ -4,7 +4,7 @@ public static partial class CommonUtility {
     public static void RemoveAll(this List<string> list,string str) {
         if (list != null && list.Count > 0)
         {
-            for (int i = list.Count - 1;i < 0;i--)
+            for (int i = list.Count - 1;i >= 0;i--)
             {
                 if (list[i] == str)
                 {
