3b7738121fea35500c5162f42aef6d5d5b4bf6a0
‘yangkangjie’ <569155228@qq.com>
2022-08-12 22:15:32 +0800

    <通用代码>

--- Assets/Script/Packages/FW_Common/Other/CommonUtility.cs
+++ Assets/Script/Packages/FW_Common/Other/CommonUtility.cs
@@ -1,4 +1,6 @@
-﻿public static partial class CommonUtility {
+﻿using System;
+
+public static partial class CommonUtility {
     public static bool IsNull(UnityEngine.Object o)
     {
         return o == null;
@@ -7,5 +9,12 @@
     public static bool IsNullOrEmpty(string txt)
     {
         return string.IsNullOrEmpty(txt);
+    }
+    
+    public static void Swap(float a,float b)
+    {
+        float swap = a;
+        a = b;
+        b = swap;
     }
 }
