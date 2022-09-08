3fc52999bb7044168617928623df17df50655aee
‘yangkangjie’ <569155228@qq.com>
2022-09-08 21:36:54 +0800

    <B+树>1.增加B+树节点和树

--- Assets/Script/A&DS/DataStruct/Tree/BTree/BPlusTree/BPlusTree.cs
+++ Assets/Script/A&DS/DataStruct/Tree/BTree/BPlusTree/BPlusTree.cs
@@ -1,8 +1,51 @@
+using System;
+using Common.OneWayChainList;
+using DataStruct.Tree.BTree.Base;
+
 namespace DataStruct.Tree.BTree.BPlusTree
 {
-    public class BPlusTree
+    public class BPlusTree<T>:BTreeBase<T>
     {
         #region 数据
+        public BPlusTree(int order,Func<T,T,int> compareFunc):base(order,compareFunc)
+        {
+            Root = new BPlusTreeNode<T>(order,true,compareFunc,this);
+        }
+        #endregion
+        
+        #region 插入
+        public override void Insert(T data)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+        
+        #region 删除
+        public override void Delete(T data)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+
+        #region 遍历
+        public override void Traversal(ref OneWayChainList<T> traversaList, Action<T> action = null)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+
+        #region 查找
+        public override BTreeNodeBase<T> FindValue(T data)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+
+        #region 查询
+        public override bool HaveValue(T data)
+        {
+            throw new NotImplementedException();
+        }
         #endregion
     }
 }
--- Assets/Script/A&DS/DataStruct/Tree/BTree/BPlusTree/BPlusTreeNode.cs
+++ Assets/Script/A&DS/DataStruct/Tree/BTree/BPlusTree/BPlusTreeNode.cs
@@ -1,81 +1,84 @@
 using System;
 using Common.OneWayChainList;
-using DataStruct.Tree.BTree.BTree;
+using DataStruct.Tree.BTree.Base;
 
 namespace DataStruct.Tree.BTree.BPlusTree
 {
-    public class BPlusTreeNode<T>
+    public class BPlusTreeNode<T>:BTreeNodeBase<T>
     {
-        // #region 数据
-        // //下一个节点
-        // public BPlusTreeNode<T> NextNode;
-        // public BPlusTreeNode<T>[] childs;
-        // #endregion
-        //
-        // #region 构造
-        // public BPlusTreeNode(int order,bool isLeaf,Func<T,T,int> compareFunc,BTree<T> tree,int count = 0):base(order,isLeaf,compareFunc,tree,count)
-        // {
-        //     
-        // }
-        // #endregion
-        //
-        // #region 查找
-        // public BPlusTreeNode<T> FindNode(T data)
-        // {
-        //     if(compareFunc == null)
-        //     {
-        //         return null;
-        //     }
-        //     
-        //     int i = 0;
-        //     while(i < count && compareFunc(data,Values[i]) >= 0)
-        //     {
-        //         i++;
-        //     }
-        //     if(isLeaf)
-        //     {
-        //         if(Values[i].Equals(data))
-        //             return this;
-        //         return null;
-        //     }
-        //
-        //     return childs[i].FindNode(data);
-        // }
-        // #endregion
-        //
-        // #region 遍历
-        // public override void Traversal(ref OneWayChainList<T> traversaList, Action<T> action = null)
-        // {
-        //     
-        // }
-        //
-        // private BPlusTreeNode<T> FindLeftNode()
-        // {
-        //     if(isLeaf)
-        //     {
-        //         return this;
-        //     }
-        //     if(childs.Length <= 0)
-        //     {
-        //         return null;
-        //     }
-        //
-        //     return childs[0].FindNode();
-        // }
-        // #endregion
-        //
-        // #region 插入
-        // public override void InserData(T data)
-        // {
-        // }
-        // #endregion
-        //
-        // #region 删除
-        //
-        // public override void DeleteData(T data)
-        // {
-        //     
-        // }
-        // #endregion
+        #region 构造
+        public BPlusTreeNode(int order,bool isLeaf,Func<T,T,int> compareFunc,BTreeBase<T> tree,int count = 0):base(order,isLeaf,compareFunc,tree,count)
+        {
+            
+        }
+        #endregion
+
+        #region 查找
+        public override BTreeNodeBase<T> FindNode(T data)
+        {
+            if(compareFunc == null)
+            {
+                return null;
+            }
+
+            int i = 0;
+            while (i < count && compareFunc(data,Values[i]) > 0)
+            {
+                i++;
+            }
+            
+            if(isLeaf)
+            {
+                if(data.Equals(Values[i]))
+                {
+                    return this;
+                }
+
+                return null;
+            }
+
+            return childs[i].FindNode(data);
+        }
+        #endregion
+        
+        #region 遍历
+        public override void Traversal(ref OneWayChainList<T> traversaList, Action<T> action = null)
+        {
+            
+        }
+        #endregion
+
+        #region 插入
+        public override void InserData(T data)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+        
+        #region 删除
+        public override void DeleteData(T data)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+
+        #region 查询
+        public override bool IsMinCount()
+        {
+            throw new NotImplementedException();
+        }
+        
+        public override bool IsMaxCount()
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+
+        #region 获取子节点
+        public override BTreeNodeBase<T> GetNewTreeNoe(int order, bool isLeaf, Func<T, T, int> compareFunc, BTreeBase<T> tree, int count = 0)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
     }
 }
