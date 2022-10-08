0509a1616b712d150fcba7e0d520797fd098b923
‘yangkangjie’ <569155228@qq.com>
2022-10-08 14:33:28 +0800

    <斐波那契堆>1.后期增加处理

--- Assets/Script/A&DS/DataStruct/Tree/Heap/FibHeap/FibHeap.cs
+++ Assets/Script/A&DS/DataStruct/Tree/Heap/FibHeap/FibHeap.cs
@@ -1,3 +1,4 @@
+//https://www.cnblogs.com/luanxm/p/10848032.html
 using System;
 
 namespace DataStruct.Tree.Heap.FibHeap
@@ -41,6 +42,8 @@ namespace DataStruct.Tree.Heap.FibHeap
                 if (compareFunc(node.data, min.data) < 0)
                     min = node;
             }
+
+            count++;
         }
         
         /// <summary>
@@ -79,7 +82,19 @@ namespace DataStruct.Tree.Heap.FibHeap
             }
         }
         
-        
+        /// <summary>
+        /// 获取最小值数据
+        /// </summary>
+        /// <returns></returns>
+        public T GetMinData()
+        {
+            if(min == null)
+            {
+                return default(T);
+            }
+
+            return min.data;
+        }
         #endregion
         
         #region 静态功能
@@ -172,10 +187,123 @@ namespace DataStruct.Tree.Heap.FibHeap
         
         public static FibHeap<T> UnionSameDegreeTree(FibHeap<T> heap)
         {
+            //计算log2(keyNum),floor向上取整
+            //例如：log2(13) = 3,向上取整：4
             int maxDegree = (int) Math.Floor(Math.Log(heap.count) / Math.Log(2));
-            return null;
+            int D = maxDegree + 1;
+            Node<T>[] unionArray = new Node<T>[D + 1];
+            
+            //不断取出当前斐波那契堆中的最小节点，然后依次进行合并操作
+            while(heap.min != null)
+            {
+                Node<T> node = RemoveMinTree(heap); //取出堆中最小树
+                if(node != null)
+                {
+                    int d = node.degree;
+                    while(unionArray[d] != null)
+                    {
+                        Node<T> sameNode = unionArray[d];
+                        if(heap.compareFunc(node.data,sameNode.data) > 0)
+                        {
+                            //保证截取的最小值比存储的要小
+                            Node<T> temp = node;
+                            node = sameNode;
+                            sameNode = temp;
+                        }
+                        
+                        Link(sameNode,node);
+                        unionArray[d] = null;
+                        unionArray[node.degree] = node;
+                    }
+
+                    unionArray[d] = node;
+                }
+            }
+
+            heap.min = null;
+            
+            for(int i  = 0;i < D;i++)
+            {
+                if(unionArray[i] != null)
+                {
+                    if(heap.min == null)
+                    {
+                        heap.min = unionArray[i];
+                    }
+                    else
+                    {
+                        AddNode(unionArray[i],heap.min);
+                        if(heap.compareFunc(unionArray[i].data,heap.min.data) < 0)
+                        {
+                            heap.min = unionArray[i];
+                        }
+                    }
+                }
+            }
+
+            return heap;
+        }
+        
+        /// <summary>
+        /// 移除最小节点
+        /// </summary>
+        /// <param name="heap"></param>
+        public static void RemoveMinNode(FibHeap<T> heap)
+        {
+            if(heap.min == null)
+            {
+                return;
+            }
+            
+            //将min下所有的儿子节点都添加到跟链表中
+            while(heap.min.child != null)
+            {
+                Node<T> child = heap.min.child;
+                
+                RemoveNode(child);
+                //如果缓存数据是最后一个数据，则清空子节点,否则右侧节点为链表开头的最新孩子节点
+                if(child.right == child)
+                {
+                    heap.min.child = null;
+                }
+                else
+                {
+                    heap.min.child = child.right;
+                }
+                
+                AddNode(child,heap.min);
+                child.parent = null;
+            }
+            
+            //将最小节点从链表中移除
+            RemoveNode(heap.min);
+            //如果min是链表的最后节点，则说明删除的是堆中最后一个数据
+            if(heap.min.right == heap.min)
+            {
+                heap.min = null;
+            }
+            else
+            {
+                heap.min = heap.min.right;
+                UnionSameDegreeTree(heap);
+            }
+
+            heap.count--;
+        }
+        
+        /// <summary>
+        /// 减少度
+        /// </summary>
+        /// <param name="parent"></param>
+        /// <param name="degree"></param>
+        private static void RenewDegree(Node<T> parent,int degree)
+        {
+            parent.degree -= degree;
+            if(parent.parent != null)
+            {
+                RenewDegree(parent.parent,degree);
+            }
         }
         #endregion
-        
     }
 }
