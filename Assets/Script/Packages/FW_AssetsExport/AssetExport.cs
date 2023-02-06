a7df838e80812d27b3a17284aa9e9a0b32a5f5af
yangkangjie <569155228@qq.com>
2023-02-06 20:27:41 +0800

    <地图资源>1.上传地图资源文件   2.上传地图编辑工具

---
+++ Assets/Script/Packages/FW_AssetsExport/AssetExport.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.Experimental;
+using UnityEngine;
+
+public class AssetExport : AssetsModifiedProcessor
+{
+    protected override void OnAssetsModified(string[] changedAssets, string[] addedAssets, string[] deletedAssets,
+        AssetMoveInfo[] movedAssets)
+    {
+        Debug.LogError("资源设置变动");
+    }
+}
